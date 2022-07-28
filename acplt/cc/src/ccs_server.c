268fb02867e0c616ce3fda051d12e4fd5b3b0300
Julian Grothoff <j.grothoff@plt.rwth-aachen.de>
2022-07-28 13:15:31 +0200

    Add some datatypes for SHM IOs in UA Server

--- acplt/cc/src/ccs_server.c
+++ acplt/cc/src/ccs_server.c
@@ -94,21 +94,24 @@ readIO(UA_Server *server, const UA_NodeId *sessionId, void *sessionContext, cons
           UA_Boolean sourceTimeStamp, const UA_NumericRange *range, UA_DataValue *dataValue) {
     CCS_IO_SHMVARIABLE* shmVar = (CCS_IO_SHMVARIABLE*) nodeContext;
     CCS_IO_SHMANY shmValue = {0};
-    if(!ccs_io_readValue(shmVar->address, &shmValue)) {
-        dataValue->hasValue = false;
-        return UA_STATUSCODE_BADNODATA;
+    if(ccs_io_readValue(shmVar->address, &shmValue)) {
+        dataValue->hasValue = true;
     }
 
     UA_StatusCode result = UA_STATUSCODE_GOOD;
     switch (shmVar->datatype) {
-        case CCS_IO_SHMDATATYPE_BOOL:
-            result = UA_Variant_setScalarCopy(&dataValue->value, (void*)&shmValue.boolean, &UA_TYPES[UA_TYPES_BOOLEAN]);
-            break;
+        case CCS_IO_SHMDATATYPE_INT:
+            return UA_Variant_setScalarCopy(&dataValue->value, (void*)&shmValue.integer, &UA_TYPES[UA_TYPES_INT32]);
+        case CCS_IO_SHMDATATYPE_UINT:
+            return UA_Variant_setScalarCopy(&dataValue->value, (void*)&shmValue.uint, &UA_TYPES[UA_TYPES_UINT32]);
+        case CCS_IO_SHMDATATYPE_REAL:
+            return UA_Variant_setScalarCopy(&dataValue->value, (void*)&shmValue.real, &UA_TYPES[UA_TYPES_FLOAT]);
         default:
-            return UA_STATUSCODE_BADTYPEMISMATCH;
+        case CCS_IO_SHMDATATYPE_BOOL:
+            return UA_Variant_setScalarCopy(&dataValue->value, (void*)&shmValue.boolean, &UA_TYPES[UA_TYPES_BOOLEAN]);
+        case CCS_IO_SHMDATATYPE_STRING:
+            return UA_STATUSCODE_BADNOTIMPLEMENTED;
     }
-    dataValue->hasValue = true;
-    return result;
 }
 
 static UA_StatusCode
@@ -120,6 +123,17 @@ writeIO(UA_Server *server, const UA_NodeId *sessionId, void *sessionContext, con
         case CCS_IO_SHMDATATYPE_BOOL:
             shmValue.boolean = *((bool*)(data->value.data));
             break;
+        case CCS_IO_SHMDATATYPE_INT:
+            shmValue.integer = *((UA_Int32*)(data->value.data));
+            break;
+        case CCS_IO_SHMDATATYPE_UINT:
+            shmValue.uint = *((UA_UInt32*)(data->value.data));
+            break;
+        case CCS_IO_SHMDATATYPE_REAL:
+            shmValue.real = *((UA_Float*)(data->value.data));
+            break;
+        case CCS_IO_SHMDATATYPE_STRING:
+            return UA_STATUSCODE_BADNOTIMPLEMENTED;
         default:
             return UA_STATUSCODE_BADTYPEMISMATCH;
     }
